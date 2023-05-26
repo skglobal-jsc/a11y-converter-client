2b2a5e6957d99ad5deb454ff4ba0cb40ecb070f1
Bao Dang <dxhbao@sk-global.biz>
2023-05-26 15:13:11 +0700

    Improve lib

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import {
   ragtJson2A11Y,
-  html2EditorJson,
-  tinyhtml,
-  editorJson2RagtJson,
 } from "./modules/index";
 
 export { 
   ragtJson2A11Y, 
-  html2EditorJson, 
-  tinyhtml, 
-  editorJson2RagtJson 
 };
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,11 +1,5 @@
-import html2EditorJson from "./html2EditorJson";
 import ragtJson2A11Y from "./ragtJson2A11Y";
-import tinyhtml from "./tinyhtml";
-import editorJson2RagtJson from './editorJson2RagtJson'
 
 export { 
-  html2EditorJson, 
-  tinyhtml, 
-  editorJson2RagtJson,
   ragtJson2A11Y, 
 };
